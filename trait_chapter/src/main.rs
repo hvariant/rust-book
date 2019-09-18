b0c40b1ee3568f413d045252225504f42739d08b
Zhansong Li <lizhansong@hvariant.com>
2019-09-18 21:22:48 +1000

    traits chapter done

---
+++ trait_chapter/src/main.rs
@@ -0,0 +1,19 @@
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("long string is long");
+    let string2 = String::from("xyz");
+    let result = longest_with_an_announcement(string1.as_str(), string2.as_str(), "rust is bust");
+    println!("The longest string is {}", result);
+}
