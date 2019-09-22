aadb097d0700fed6430f6a4bdf288bd593f89eea
zhansongl <zhansong.li@ir.com>
2019-09-22 20:27:27 +1000

    trait objects

---
+++ trait_objects/src/main.rs
@@ -0,0 +1,39 @@
+mod lib;
+
+use lib::{Draw, Screen, Button};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("drawing the select box with options: {:?}", self.options);
+    }
+}
+
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
